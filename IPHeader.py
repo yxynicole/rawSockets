980c322fde65ba677bcd15a5f8acf15e1a8e0002
jtaylor617 <73145984+jtaylor617@users.noreply.github.com>
2020-11-16 00:18:36 -0500

    Update and rename ip_header.py to IPHeader.py

--- IPHeader.py
+++ IPHeader.py
@@ -5,7 +5,7 @@ from struct import *
 
 class IpHeader:
 
-    def __init__(self):
+    def __init__(self, src_ip, dest_ip):
         self.version = 4
         self.ihl = 5
         self.type_of_service = 0
@@ -15,11 +15,11 @@ class IpHeader:
         self.ttl = 255
         self.protocol = socket.IPPROTO_TCP
         self.checksum = 0
-        self.src_addr = socket.inet_aton('192.168.109.128') # my VM IP address
-        self.dest_addr = socket.inet_aton('52.70.229.197') # ccs.neu.edu ip address
+        self.src_addr = socket.inet_aton(src_ip) # my VM IP address
+        self.dest_addr = socket.inet_aton(dest_ip) # ccs.neu.edu ip address
 
 
-    def pack_data(self):
+    def form_ip_header(self):
         ihl_version = (self.version << 4) + self.ihl
         ip_header = pack('!BBHHHBBH4s4s', ihl_version, self.type_of_service, self.length, self.identification, self.frag_offset, self.ttl, self.protocol, self.checksum, self.src_addr, self.dest_addr)
         return ip_header
